fb1df6a4dee38dad10daf806b7db7d705f07e8d4
saiemgilani <saiem.gilani@gmail.com>
2021-11-06 05:54:39 -0400

    update with package docs tables

--- R/hoopR.R
+++ R/hoopR.R
@@ -88,4 +88,4 @@ shot_clock_range_df %>%
        subtitle ="Among Top 30 in FG3A  (2020-21)", 
        fill = "Seconds Remaining on Shot Clock") 
 
-  ggsave('figures/hoopR_chicklet.png', width = 5.0, height = 6.5, units=c("in"))
+  ggsave('figures/hoopR_chicklet.png', width = 5.0, height = 5.0, units=c("in"))
